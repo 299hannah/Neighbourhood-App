c5a7795cb3249d823239495d2183f6c3ff07b0e8
299hannah <hannah.wambui@student.moringaschool.com>
2021-07-25 21:19:57 +0300

    profile

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from hood.forms import ProfileForm, UserForm
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import Profile
 # Create your views here.
+
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserForm(request.POST, instance=request.user)
+        p_form = ProfileForm(request.POST, request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserForm(instance=request.user)
+        p_form = ProfileForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'profile.html', context)
